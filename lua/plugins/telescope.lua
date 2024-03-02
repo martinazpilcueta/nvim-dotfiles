5955e9869c303080fc57e813d59bb9562c235112
Martín Azpilcueta <martinazpilcueta@users.noreply.github.com>
2024-03-02 17:44:40 +0100

    add buffers and recent files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,8 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
         vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
         vim.keymap.set('n', '<leader>gs', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
