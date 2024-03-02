e36e3dd2f40fd62b446ea737a519d224b1c94acf
Martín Azpilcueta <martinazpilcueta@users.noreply.github.com>
2024-03-02 18:05:04 +0100

    add cmp cool formatting

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -9,18 +9,40 @@ return {
         local cmp = require("cmp")
     cmp.setup({
         completion = {
-            completeopt = "menu,menuone,preview,noselect"
+            completeopt = "menu,menuone,preview,noselect",
+                keyword_length = 2
         },
+            window = {
+  documentation = cmp.config.window.bordered(),
+  completion = cmp.config.window.bordered()
+},
             mapping = cmp.mapping.preset.insert({
+                ["<Up>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+                ["<Down>"] = cmp.mapping.select_next_item(), -- next suggestion
                 ["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion
                 ["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
                 ["<C-Space>"] = cmp.mapping.complete(),
+                ["<CR>"] = cmp.mapping.confirm({select = false}),
                 ["<C-e>"] = cmp.mapping.abort(),
             }),
             sources = cmp.config.sources({
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
-      })
+      }),
+            formatting = {
+  fields = {'menu', 'abbr', 'kind'},
+  format = function(entry, item)
+    local menu_icon = {
+      nvim_lsp = 'λ',
+      luasnip = '⋗',
+      buffer = 'Ω',
+      path = '🖫',
+    }
+
+    item.menu = menu_icon[entry.source.name]
+    return item
+  end,
+},
         })
     end
     }
