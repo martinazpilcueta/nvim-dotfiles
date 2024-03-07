53086b671b48d9b628559a72fa7726daf56c7dff
Martín Azpilcueta <martinazpilcueta@users.noreply.github.com>
2024-03-07 19:49:14 +0100

    add ctrl j and k for windows

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
-vim.keymap.set({"n", "v"}, "<space>", "<nop>")
+vim.keymap.set({ "n", "v" }, "<space>", "<nop>")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "Q", "<nop>")
