f11f2e4c71bd49e2dee35e03c294243689450d97
Martín Azpilcueta <martinazpilcueta@users.noreply.github.com>
2024-03-07 19:50:32 +0100

    explorer quit on open

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,41 +1,52 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-        require("nvim-tree").setup {
-            renderer = {
-                icons = {
-                    glyphs = {
-                        default = "",
-                        symlink = "",
-                        folder = {
-                            arrow_open = "",
-                            arrow_closed = "",
-                            default = "",
-                            open = "",
-                            empty = "",
-                            empty_open = "",
-                            symlink = "",
-                            symlink_open = "",
-                        },
-                        git = {
-                            unstaged = "",
-                            staged = "S",
-                            unmerged = "",
-                            renamed = "➜",
-                            untracked = "U",
-                            deleted = "",
-                            ignored = "◌",
-                        },
-                    },
-                },
-            } }
-        vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
-    end,
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    require("nvim-tree").setup {
+      update_focused_file = { enable = true },
+      actions = {
+        open_file = {
+          -- Automatically close the NvimTree window after opening a file
+          quit_on_open = true,
+        },
+      },
+      view = {
+        adaptive_size = true,
+        side = "right"
+      },
+      renderer = {
+        icons = {
+          glyphs = {
+            default = "",
+            symlink = "",
+            folder = {
+              arrow_open = "",
+              arrow_closed = "",
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+              symlink_open = "",
+            },
+            git = {
+              unstaged = "",
+              staged = "S",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "U",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
+        },
+      } }
+    vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+  end,
 }
