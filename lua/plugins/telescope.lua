a8af00a588a45f449ae37e78a4633cc35524b65a
Martín Azpilcueta <martinazpilcueta@users.noreply.github.com>
2024-03-07 19:52:42 +0100

    change to grep string for live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,15 +3,19 @@ return {
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            pickers = {
+                oldfiles = {
+                    cwd_only = true,
+                }
+            },
+        })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
         vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>gs', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set('n', '<leader>gs', builtin.live_grep, {})
     end
 }
